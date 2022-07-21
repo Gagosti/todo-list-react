8f0aa0c669bea84c5e395f3841ac64bcb09ecf28
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-21 17:03:45 -0300

    deleting items

--- todo/src/List.js
+++ todo/src/List.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function List(props) {
+
+  function deleteItem(item) {
+    console.log(item)
+  }
+
   return(
   <ul>
-    {props.items.map(item=><li key={item.id}>{item.text}</li>)}
+    {props.items.map(item=><li key={item.id}>
+      {item.text}
+      <button onClick={()=>{props.onItemDeleted(item)}}><img alt="delete" src="./assets/bin.png"></img></button>
+      </li>)}
   </ul>)
 }
 
