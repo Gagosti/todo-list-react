c7527f533dc1c4d45f5b179045450c827839429a
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-21 12:57:02 -0300

    useState - fixed

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import './Todo.css'
+
 function Todo() {
 
-  const [text, setText] = useState("");
+  const [text, setText] = React.useState("");
 
   function handleChange(event) {
     let t = event.target.value;
     setText(t);
   }
 
-  return (<div className="container">
-      <h1>Todo</h1>
-      <form>
-        <input onChange={handleChange} type="text"></input>
-        <button>Add</button>
-      </form>
-      <ul>
-        <li>{text}</li>
-      </ul>
-    </div> )
+  return (
+  <div className='container'>
+    <h1>Todo</h1>
+    <form>
+      <input onChange={handleChange} type="text"></input>
+      <button>Add</button>
+    </form>
+    <ul>
+      <li>{text}</li>
+    </ul>
+  </div>)
 }
-
 
 export default Todo
